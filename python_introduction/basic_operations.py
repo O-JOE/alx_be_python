ab2fbf42e0f505880486bceada8df5d04b538356
joseph ekow otoo <josephekowotoo@gmail.com>
2025-11-09 19:50:36 +0000

    basic_operations in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print("Addition of 10 and 5 is" , sum)
+diff = number1 - number2
+print("Subtraction of 10 and 5 is" , diff)
+times = number1 * number2
+print("Multiplication of 10 and 5 is" , times)
